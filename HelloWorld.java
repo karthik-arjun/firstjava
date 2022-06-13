7f2d5a9cf5f0acb729229c91b6b1abbb942c79f1
karthik-arjun <77783376+karthik-arjun@users.noreply.github.com>
2022-06-13 13:24:21 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,5 @@
-class HelloWorld
-{
-   public static void main(String args[])
-   {
-      System.out.println("Hello World");
+public class Hello {
+   public static void main(String[] args) {
+      System.out.println("Hello, World");
    }
 }
